eb7f1a15f716514c0b012b69cbb80ea259b72128
Azulax-Monk <maksym.khavro@gmail.com>
2024-01-27 05:49:28 +0200

    Add table-map props

---
+++ Assets/Scripts/InteractableObjects/Props/MapTable.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InteractableObjects
+{
+    public class MapTable : OutlinedInteractable
+    {
+        public override void Interact(GameObject character)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        // Start is called before the first frame update
+        protected override void Start()
+        {
+            base.Start();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
