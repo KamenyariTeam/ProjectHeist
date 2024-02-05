2db499298cb708bfa00aa14390c2bc596db17ea1
Viktor Pavlenchyk <vpavlenchik@gmail.com>
2024-02-06 01:15:55 +0200

    Moved all pickups in one file

--- Assets/Scripts/InteractableObjects/Pickups/Pickup.cs
+++ Assets/Scripts/InteractableObjects/Pickups/Pickup.cs
@@ -1,4 +1,6 @@
+using Characters.Player;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace InteractableObjects.Pickups
 {
@@ -7,6 +9,40 @@ namespace InteractableObjects.Pickups
         public override void Interact(GameObject character)
         {
             Destroy(gameObject);
+        }
+    }
+
+    public class MedKit : Pickup
+    {
+        public float healthRegeneration = 25f;
+
+        public override void Interact(GameObject character)
+        {
+            base.Interact(character);
+
+            HealthComponent healthComponent = character.GetComponent<HealthComponent>();
+
+            if (healthComponent != null)
+            {
+                healthComponent.TakeMedicine(healthRegeneration);
+            }
+        }
+    }
+
+    public class Armor : Pickup
+    {
+        [FormerlySerializedAs("ArmorRegeneration")] public float armorRegeneration = 25f;
+
+        public override void Interact(GameObject character)
+        {
+            base.Interact(character);
+
+            HealthComponent healthComponent = character.GetComponent<HealthComponent>();
+
+            if (healthComponent != null)
+            {
+                healthComponent.TakeArmor(armorRegeneration);
+            }
         }
     }
 }
