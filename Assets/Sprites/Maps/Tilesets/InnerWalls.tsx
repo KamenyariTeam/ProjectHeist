bae2ad58eb050dd35ab436bd7045c1052c12394e
Vitalii Fedorchak <vetalfed58@gmail.com>
2024-03-25 21:05:42 +0200

    Map for level 1

---
+++ Assets/Sprites/Maps/Tilesets/InnerWalls.tsx
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="InnerWalls" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="Inner_tileset.png" width="128" height="128"/>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="25" width="32" height="7"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="25" y="0" width="7" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="25" y="0" width="7" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="25" width="32" height="7"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="25" y="25" width="7" height="7"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index" id="2">
+   <object id="4" x="25.087" y="0" width="6.91304" height="32"/>
+   <object id="5" x="0" y="25.0435" width="25.1304" height="6.95652"/>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="25" width="32" height="7"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="25" width="32" height="7"/>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="25" y="0" width="7" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="25" y="0" width="7" height="32"/>
+  </objectgroup>
+ </tile>
+</tileset>
