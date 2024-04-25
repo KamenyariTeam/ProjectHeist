d92b7e6319509e7809ba20a93b5a645a0a325711
Azulax-Monk <maksym.khavro@gmail.com>
2024-04-25 22:44:38 +0300

    Reework door movement

--- Assets/Scripts/InteractableObjects/Door/Door.cs
+++ Assets/Scripts/InteractableObjects/Door/Door.cs
@@ -6,14 +6,16 @@ namespace InteractableObjects.Door
 {
     public class Door : OutlinedInteractable
     {
-        private static readonly int IsOpen = Animator.StringToHash("IsOpen");
-
         [SerializeField] private float maxTimeOpened;
 
-        [SerializeField] private List<Animator> doorAnimators;
+        private Vector3 _startPosition;
+        private Vector3 _endPosition;
+        private float _movementTime = 1.0f; // Duration of the slide
+        private float _timer;
+        private float _timeOpened = 0.0f;
+        private bool _shouldSlide = false;
+        private bool _isOpened = false;
 
-        private bool _isOpened;
-        private float _timeOpened;
 
         public override void Interact(GameObject interacter)
         {
@@ -27,8 +29,6 @@ namespace InteractableObjects.Door
         protected override void Start()
         {
             base.Start();
-            _isOpened = false;
-            _timeOpened = 0.0f;
         }
 
         private void Update()
@@ -41,15 +41,35 @@ namespace InteractableObjects.Door
                     ChangeState();
                 }
             }
+
+            if (_shouldSlide)
+            {
+                if (_timer < _movementTime)
+                {
+                    _timer += Time.deltaTime;
+                    transform.position = Vector3.Lerp(_startPosition, _endPosition, _timer / _movementTime);
+                }
+                else
+                {
+                    _shouldSlide = false;
+                }
+            }
         }
 
         private void ChangeState()
         {
+            // Set position for the opposite direction
+            float dir = _isOpened ? 1.0f : -1.0f;
+
+            // TODO
+            // Get correct renderer of movable door (Square, not Mirrage)
+            float width = GetComponent<Renderer>().bounds.size.x;
+            _startPosition = transform.position;
+            _endPosition = _startPosition + dir * transform.right * width;
+
             _isOpened = !_isOpened;
-            foreach (var animator in doorAnimators)
-            {
-                animator.SetBool(IsOpen, _isOpened);
-            }
+            _timer = 0;
+            _shouldSlide = true;
             _timeOpened = 0.0f;
         }
     }
