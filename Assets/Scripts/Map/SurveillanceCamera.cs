c76bb841cd640cc2acec61c679e9c6fbd614dc69
Vitalii Fedorchak <vetalfed58@gmail.com>
2024-07-23 19:10:06 +0300

    Package updates + camera final

--- Assets/Scripts/Map/SurveillanceCamera.cs
+++ Assets/Scripts/Map/SurveillanceCamera.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Characters.Player;
 using UnityEngine;
 
 namespace Map
@@ -16,7 +17,8 @@ namespace Map
         [SerializeField] private Color detectedStateColor = Color.red;
 
         [Header("References")]
-        [SerializeField] private Transform playerTransform; // Reference to the player's transform
+        private Transform _playerTransform;
+        private StealthComponent _playerStealthComponent;
 
         private float _initialRotation;
         private bool _isRotatingRight;
@@ -26,19 +28,28 @@ namespace Map
 
         private void OnValidate()
         {
-            InitializeMesh(); // Regenerate the view cone mesh when values change in the Inspector
+            if (!Application.isPlaying && Application.isEditor)
+                InitializeMesh(); // Regenerate the view cone mesh when values change in the Inspector
         }
 
         private void Start()
         {
-            _initialRotation = transform.eulerAngles.z;
-            _isRotatingRight = true;
-            _playerDetected = false;
-
+            InitializeReferences();
             InitializeMesh();
         }
 
-        void InitializeMesh()
+        private void InitializeReferences()
+        {
+            var player = GameObject.FindGameObjectWithTag("Player");
+            _playerTransform = player.transform;
+            _playerStealthComponent = player.GetComponent<StealthComponent>();
+
+            _initialRotation = transform.eulerAngles.z;
+            _isRotatingRight = true;
+            _playerDetected = false;
+        }
+
+        private void InitializeMesh()
         {
             MeshFilter meshFilter = GetComponentInChildren<MeshFilter>();
             _meshRenderer = GetComponentInChildren<MeshRenderer>();
@@ -69,7 +80,6 @@ namespace Map
             DetectPlayer();
         }
 
-        // Rotate the camera back and forth within the specified range
         private void RotateCamera()
         {
             float rotationSpeedAdjusted = _isRotatingRight ? -rotationSpeed * Time.deltaTime : rotationSpeed * Time.deltaTime;
@@ -83,10 +93,9 @@ namespace Map
             }
         }
 
-        // Follow the player by smoothly rotating the camera towards them
         private void FollowPlayer()
         {
-            Vector3 directionToPlayer = playerTransform.position - transform.position;
+            Vector3 directionToPlayer = _playerTransform.position - transform.position;
             float angleToPlayer = Mathf.Atan2(directionToPlayer.y, directionToPlayer.x) * Mathf.Rad2Deg - 90f;
             float step = rotationSpeed * Time.deltaTime;
             float newRotation = Mathf.MoveTowardsAngle(transform.eulerAngles.z, angleToPlayer, step);
@@ -136,35 +145,45 @@ namespace Map
         // Detect the player within the camera's view cone
         private void DetectPlayer()
         {
-            Vector3 directionToPoint = playerTransform.position - transform.position;
-            float distanceToPoint = directionToPoint.magnitude;
-
-            if (distanceToPoint > viewDistance || distanceToPoint < deadZoneDistance)
+            if (!_playerStealthComponent.IsNoticeable)
             {
-                SetSearchState();
+                SetSearchState(); // Player is not noticeable, continue searching
                 return;
             }
 
-            float halfAngle = viewAngle / 2.0f;
-            float angleToPoint = Vector3.Angle(transform.up, directionToPoint);
+            Vector3 directionToPlayer = _playerTransform.position - transform.position;
+            float distanceToPlayer = directionToPlayer.magnitude;
 
-            if (Mathf.Abs(angleToPoint) <= halfAngle)
+            if (IsPlayerOutOfView(directionToPlayer, distanceToPlayer) || 
+                IsObstacleBlockingView(directionToPlayer, distanceToPlayer))
             {
-                RaycastHit2D hit = Physics2D.Raycast(transform.position, directionToPoint, distanceToPoint, obstacleMask);
-                if (!hit.collider)
-                {
-                    SetDetectedState();
-                    Debug.Log("Player detected!");
-                }
-                else
-                {
-                    SetSearchState();
-                }
+                SetSearchState(); // Player is out of view or an obstacle is blocking, continue searching
             }
             else
             {
-                SetSearchState();
+                SetDetectedState(); // Player detected
+                Debug.Log("Player detected!");
             }
+        }
+
+        // Check if the player is out of the camera's view range or view cone
+        private bool IsPlayerOutOfView(Vector3 directionToPlayer, float distanceToPlayer)
+        {
+            if (distanceToPlayer > viewDistance || distanceToPlayer < deadZoneDistance)
+            {
+                return true; // Player is out of the view distance range
+            }
+
+            float halfViewAngle = viewAngle / 2.0f;
+            float angleToPlayer = Vector3.Angle(transform.up, directionToPlayer);
+            return Mathf.Abs(angleToPlayer) > halfViewAngle; // Player is out of the view cone angle
+        }
+
+        // Check if an obstacle is blocking the view to the player
+        private bool IsObstacleBlockingView(Vector3 directionToPlayer, float distanceToPlayer)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, directionToPlayer, distanceToPlayer, obstacleMask);
+            return hit.collider != null; // There is an obstacle blocking the view
         }
 
         private void SetSearchState()
